5d5adf01973f3db38db325d464932ae0d242249f
scupak <antonibujko@hotmail.com>
2023-05-30 22:45:25 +0200

    making stored procedures

---
+++ DBD-Exam-Synopsis-Project/IStoredProcedures.cs
@@ -0,0 +1,25 @@
+﻿namespace DBD_Comp_StoredProcedures
+{
+    internal interface IStoredProcedures
+    {
+        int CreatePerson(string PName);
+
+        void CreatePersonFriend(int PId1, int PId2);
+
+        void GetAllPersons();
+
+        void GetPerson(int PId);
+
+        void GetAllFriendsOfPerson(string PName);
+
+        void GetAllFriendsOfPersonReciprocal(string PName);
+
+        void usp_GetAllFriendsOfFriendsPerson(string PName);
+
+        int UpdatePersonName(int PId, string PName);
+
+        int DeletePerson(int PId);
+
+
+    }
+}
