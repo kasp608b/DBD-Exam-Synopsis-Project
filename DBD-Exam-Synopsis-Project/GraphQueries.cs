1adbfd7d257cdae8a8c70fdefcbc5b7963c804b5
kasp608b <54952896+kasp608b@users.noreply.github.com>
2023-06-06 20:21:05 +0200

    Update GraphQueries.cs

--- DBD-Exam-Synopsis-Project/GraphQueries.cs
+++ DBD-Exam-Synopsis-Project/GraphQueries.cs
@@ -1,14 +1,5 @@
 ﻿using ConsoleTables;
 using Neo4j.Driver;
-using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Linq;
-using System.Reflection.PortableExecutable;
-using System.Security.Cryptography;
-using System.Security.Policy;
-using System.Text;
-using System.Threading.Tasks;
 namespace DBD_Exam_Synopsis_Project
 {
 
